74e6049aa4e7b0c6d7847fb93bf528f1cf73b79f
victorlaiyeeteng <vlai69863@gmail.com>
2024-12-02 21:30:39 +0800

    updated credentials

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://vinitshahdeo.com",
+  site: "https://none.com",
   integrations: [
     tailwind(),
     icon(),
